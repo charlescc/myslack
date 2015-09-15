fba5fa1ed60d08a7caef73abaff3e24df44b9b6c
charles <314950350@qq/com>
2015-09-16 01:13:18 +0800

    login complished

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 var app=angular.module('myModule',[]);
 app.controller('myController',function($scope){
-	$scope.loginInfo=[{
-		key:'username',
-		placeholder:'Your User Name',
+	$scope.user={
+		key:'nickname',
+		
 		description:'以_或者字母数字组成',
-		pattern:''，
-	}];
+		pattern:/^[_\w]+$/
+	};
 
 });
